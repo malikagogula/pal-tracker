952389e67360025cd43912450cee067885b09e10
Alex Sartan <asartan@pivotal.io>
2018-01-26 10:30:42 -0700

    env variables 2.0

--- test/PalTrackerTests/IntegrationTestServer.cs
+++ test/PalTrackerTests/IntegrationTestServer.cs
@@ -6,6 +6,7 @@ namespace PalTrackerTests
 {
     public static class IntegrationTestServer
     {
+<<<<<<< HEAD
         public static TestServer Start()
         {
             if (string.IsNullOrEmpty(System.Environment.GetEnvironmentVariable("WELCOME_MESSAGE")))
@@ -14,5 +15,9 @@ namespace PalTrackerTests
             }
             return new WebApplicationFactory<Program>().Server;
         }
+=======
+        public static TestServer Start() =>
+            new WebApplicationFactory<Program>().Server;
+>>>>>>> e7f07dd... Add tests for externalized configuration lab
     }
 }
