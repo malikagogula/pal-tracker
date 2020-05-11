0f0e99b44db990df7fca8bc76f63bf2e23fdfaec
Malika Gogula <malikagogula@gmail.com>
2020-05-11 10:05:52 +0000

    Added environment variables

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -26,6 +26,15 @@ namespace PalTracker
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            var message = Configuration.GetValue<string>("WELCOME_MESSAGE");
+            if (string.IsNullOrEmpty(message))
+            {
+            throw new ApplicationException("WELCOME_MESSAGE not configured.");
+            }
+            services.AddSingleton(sp => new WelcomeMessage(message));
+
+            services.AddSingleton(sp => new CloudFoundryInfo("123","512M","1", "127.0.0.1"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
