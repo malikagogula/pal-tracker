2f946d5ab2accb8d5be7af89eddba8e540695a3a
Malika Gogula <malikagogula@gmail.com>
2020-05-13 04:33:32 +0000

    Health monitor

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -11,6 +11,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
+using Steeltoe.Management.CloudFoundry;
+using Steeltoe.Management.Endpoint.CloudFoundry;
+using Steeltoe.Common.HealthChecks;
+using Steeltoe.Management.Endpoint.Info;
+using Steeltoe.Management.Endpoint;
+using Steeltoe.Management.Hypermedia;
 
 namespace PalTracker
 {
@@ -26,6 +32,8 @@ namespace PalTracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+            services.AddCloudFoundryActuators(Configuration, MediaTypeVersion.V2, ActuatorContext.ActuatorAndCloudFoundry);
             services.AddControllers();
 
             var message = Configuration.GetValue<string>("WELCOME_MESSAGE");
@@ -40,6 +48,9 @@ namespace PalTracker
             //services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
             services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
             services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
+            services.AddScoped<IHealthContributor, TimeEntryHealthContributor>();
+            services.AddSingleton<IOperationCounter<TimeEntry>, OperationCounter<TimeEntry>>();
+            services.AddSingleton<IInfoContributor, TimeEntryInfoContributor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +60,8 @@ namespace PalTracker
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCloudFoundryActuators(MediaTypeVersion.V2, ActuatorContext.ActuatorAndCloudFoundry);
 
             app.UseHttpsRedirection();
 
